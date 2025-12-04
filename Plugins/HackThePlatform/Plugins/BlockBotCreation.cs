edd22ccdfa889cc59ae64011454fd5948b4f31c5
dudelis <fukszon.kostya@gmail.com>
2025-12-04 22:28:26 +0100

    adds BlockBotCreation plugin

---
+++ Plugins/HackThePlatform/Plugins/BlockBotCreation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugins
+{
+    public class BlockBotCreation : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new InvalidPluginExecutionException(
+                "Copilot Agents creation is not allowed in the Default environment."
+            );
+        }
+    }
+}
